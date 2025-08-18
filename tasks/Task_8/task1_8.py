ca835d64f036114400178e25b6cc3f74978f17b0
Nababa Tee <nababatee@gmail.com>
2025-08-18 12:57:21 +0100

    new task

---
+++ tasks/Task_8/task1_8.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter first number: ")) # this  collects a number from the user
+num2 = int(input("Enter second number: ")) # this collects another number feom the user
+
+print(f"{num1} == {num2} : {num1 == num2}")
+# the output will be true if num1 is equal to num2 otherwise false
+print(f"{num1} != {num2} : {num1 != num2}")
+# the output will be true if num1 is  not equal to num2 otherwise false
+print(f"{num1} > {num2} : {num1 > num2}")
+# the output will be true if num1 is  greater than  num2 otherwise false
+print(f"{num1} < {num2} : {num1 < num2}")
+# the output will be true if num1 is  less than  num2 otherwise false
+
+# Use cases
+Student Exam Result
+Physical Exam Result
---
+++ tasks/Task_8/task2_8.py
@@ -0,0 +1,15 @@
+# name = input("Enter your name: ") # collects the name of the users
+# age = int(input("Enter your age: "))# collects the age of the users
+# score = int(input("Enter your test score: ")) # collects the test score of the users
+# # Must be younger than 18 AND score above 80
+# eligibility = (age < 18) and (score > 70) 
+# # Outpu: candidate's name, age, score, and eligibilty status
+# print(f"Candidate: {name}\nAge: {age}\nScore: {score}\nEligible: {eligibility}")
+
+print("\nFederal Government Scholarship Key Eligibility Requirements:")
+name = input("Enter name: ")# collects the name of the users
+nationality = input("Enter your nationality: ")# collects the nationality of the users
+employment_status = input("Enter employment status(undergrad/grad/employed): ")# collects the employment status of the users
+Scholar_enrolled = input("Are you enrolled in other schorlaship from an entity in the Oil and Gas industry: ")# collects the onformation on schorlaship they are enrolled in
+Academic_qualific = input("Enter your academic qualification: ")
+
