79459662fe411c2d049655f6fde6140b723190dc
Nababa Tee <nababatee@gmail.com>
2025-08-18 15:25:37 +0100

    jjj

--- tasks/Task_8/task2_8.py
+++ tasks/Task_8/task2_8.py
@@ -7,13 +7,14 @@
 # print(f"Candidate: {name}\nAge: {age}\nScore: {score}\nEligible: {eligibility}")
 
 print("\nFederal Government Scholarship Key Eligibility Requirements:")
-name = input("Enter name: ").tittle()# collects the name of the users
-nationality = input("Enter your nationality: ").title()# collects the nationality of the users
-employment_status = input("Enter employment status(undergrad/grad/employed): ")# collects the employment status of the users
+name = input("Enter name: ").lower()# collects the name of the users
+nationality = input("Enter your nationality: ").lower()# collects the nationality of the users
+employment_status = input("Enter employment status(undergrad/grad/employed): ").lower()# collects the employment status of the users
 Scholar_enrolled = input("Are you enrolled in other schorlaship from an entity in the Oil and Gas industry: ").lower()# collects the onformation on schorlaship they are enrolled in
 Academic_subject = input("Enter 5 subjects separated by comma: ")
 academic_qualiafication = [input(f"Enter grade for {subject}: ") for subject in Academic_subject]
+accepeted_grade = ("A" in academic_qualiafication) or ("B" in academic_qualiafication)
 
-eligibility = (nationality == "Nigerian") and (employment_status == )
+eligibility = (nationality == "nigerian") and (employment_status == "undergraduate") and (Scholar_enrolled == "no") and (accepeted_grade = True)
 
 
